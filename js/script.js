ac621a88623e18899092ee135907f7bbcf38db79
rittobabu <ritto.babu@hudini.io>
2022-02-26 01:58:08 +0530

    foucus style

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@
   $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     //console.log(scroll);
-    if (scroll > 200) {
+    if (scroll > 65) {
       //console.log('a');
       $('.navigation').addClass('sticky-header');
     } else {
